9f534a7ab6b1841cdfd7c90451304be0059b44c2
Tsai <ian01045@gmail.com>
2018-04-14 22:56:14 +0800

    branch1666666

--- HomePage/HomePage/Controllers/HomeController.cs
+++ HomePage/HomePage/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace HomePage.Controllers
 {
     public class HomeController : Controller
     {
-        //123 456  123  123  123 11  22 branch bbb
+        //123 456  123  123  123 11  22 branch bbb dd
         // GET: Home
         public ActionResult Index()
         {
