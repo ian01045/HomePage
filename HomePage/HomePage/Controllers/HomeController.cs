8155df7cf7e862fa6e4c3ef611d8b3c6ff27b39a
Tsai <ian01045@gmail.com>
2018-04-14 22:04:10 +0800

    11

--- HomePage/HomePage/Controllers/HomeController.cs
+++ HomePage/HomePage/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace HomePage.Controllers
 {
     public class HomeController : Controller
     {
-        //123 456  123  123
+        //123 456  123  123  123
         // GET: Home
         public ActionResult Index()
         {
